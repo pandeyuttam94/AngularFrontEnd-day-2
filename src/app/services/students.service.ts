9224693d99b7a1cbc4929689d9734ac8e9bd5ce1
Uttam Pandey <pandey.uttam94@gmail.com>
2018-05-16 11:08:53 +0530

    day 2

---
+++ src/app/services/students.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../Models/Student';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentsService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>("http://127.0.0.1:5555/students")
+  }
+
+  addStudents(name: string): Observable<Student> {
+    return this.http.post<Student>("http://127.0.0.1:5555/students?name=" + name, name)
+  }
+}
